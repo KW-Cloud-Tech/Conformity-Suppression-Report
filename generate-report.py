e0e2120ebe0d46cc21cace1c530b0d684d7e5898
Konrad Wallace <59604517+KW-Cloud-Tech@users.noreply.github.com>
2021-06-23 12:50:50 +1000

    Update generate-report.py

--- generate-report.py
+++ generate-report.py
@@ -13,11 +13,12 @@ from os import path
 apiKey = os.environ["CC_API_KEY"]
 
 # Read config file and make case-sensitive
+configFileName = "request_config.ini"
 config = configparser.ConfigParser()
 config.optionxform=str
-config.read("request_config.ini")
+config.read(path.join(path.dirname(sys.argv[0]), configFileName))
 
-# Load and prepare parameters
+# Load and prepare API parameters
 parameters = config["parameters"]
 region = parameters.get("conformityHostedRegion")
 accountIds = parameters.get("accountIds")
@@ -28,7 +29,7 @@ else :
 	pageNumber = 0
 parameter_string = "?accountIds={}&page[size]={}&page[Number]={}".format(accountIds, pageSize, pageNumber)
 
-# Load and prepare filters
+# Load and prepare checks API filters
 filters = config["filters"]
 filter_string = ''
 for option in filters: 
@@ -40,7 +41,7 @@ notesBoolean = note_filters.get("notes")
 notesLength = note_filters.get("notesLength")
 note_string = "?filter[notes]={}&filter[notesLength]={}".format(notesBoolean, notesLength)
 
-# Building the API query URL
+# Building the initial Checks API query URL
 checks_url = "https://{}-api.cloudconformity.com/v1/checks".format(region)
 all_checks_url = "{}{}{}".format(checks_url, parameter_string, filter_string)
 headers = {
@@ -51,6 +52,7 @@ headers = {
 # launch the request session
 session = requests.session()
 
+# function to list all checks definied by the filters
 def get_account_checks():
     combined = []
     if type(parameters.get("page[Number]")) == type(0) : 
@@ -68,19 +70,19 @@ def get_account_checks():
         combined += data
     return {"data": combined, "meta": page["meta"]}
 
+# Launch the initial list-checks API call
+print("Retrieving all relevant checks definied by {}...".format(configFileName))
+pages_combined = get_account_checks()
+print("Done\n")
+returned_checks = pages_combined["data"]
+
 # Establish a list for the final report data
 report_data = []
 
-# Query each check to extract suppression data
-print("Retrieving all relevant checks based on config file...")
-pages_combined = get_account_checks()
-print("DONE")
-returned_checks = pages_combined["data"]
-
+# Using checks data, loop through each check to retrieve further details including the supression notes
 print("Initiating process to retrieve each check's details...")
 check_count = 0
 for check in returned_checks:
-
 	checkId = check["id"]
 	specific_check_url = "{}/{}{}".format(checks_url, checkId, note_string)
 
@@ -88,7 +90,7 @@ for check in returned_checks:
 	retrieve = session.get(specific_check_url, headers=headers)
 	print("API response:" + str(retrieve.status_code) + " for check: " + checkId)
 	
-	# Build the data dictionary for the report output
+	# Build the data dictionary for the report output for each check
 	response_data = retrieve.json()
 	epoch_suppression_date = int(response_data["data"]["attributes"]["notes"][0]["created-date"])
 	local_converted_date = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(epoch_suppression_date/1000))
@@ -108,17 +110,23 @@ for check in returned_checks:
 		"resolutionPage" : response_data["data"]["attributes"]["resolution-page-url"]
 	}
 	
-	# Append check data to report data
+	# Append check data to the combined report_data list object
 	report_data.append(reportDictionary)
 	check_count += 1
 
 # Confirming report output
 totalChecks = len(report_data)
-print("Report data compiled for a total of {} checks".format(check_count))
+print("\nReport data compiled for a total of {} checks".format(totalChecks))
 
+# Prepare report filepath and timestamp
 timestamp = time.strftime('%Y-%m-%d_%H:%M:%S', time.localtime())
+filepath = path.join(path.dirname(sys.argv[0]),"suppression_report_{}.csv".format(timestamp))
+
+# Convert report_data to a csv output
 keys = report_data[0].keys()
-with open("suppression_report_{}.csv".format(timestamp), "w", newline="")  as output_file:
+with open(filepath, "w", newline="")  as output_file:
     dict_writer = csv.DictWriter(output_file, keys)
     dict_writer.writeheader()
     dict_writer.writerows(report_data)
+
+print("\nData saved as: suppression_report_{}.csv\nDone".format(timestamp))    
